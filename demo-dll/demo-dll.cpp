85eb6a3ebec1f52c402bcc94cc7d6b9bc37e7d16
alwxkxk <alwxkxk@gmail.com>
2020-01-22 21:15:09 +0800

    feat: first commit.

---
+++ demo-dll/demo-dll.cpp
@@ -0,0 +1,20 @@
+﻿// demo-dll.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+
+int returnNumber2(void) {
+	return 2;
+}
+
+char* returnString(void) {
+	static char value[20];
+	strcpy_s(value, "returnString");
+	return value;
+}
+
+int sum(int a, int b) {
+	return a + b;
+}
+
+
---
+++ demo-dll/stdafx.h
@@ -0,0 +1,19 @@
+﻿// stdafx.h: 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 项目特定的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的内容
+// Windows 头文件
+#include <windows.h>
+
+
+
+// 在此处引用程序需要的其他标头
+extern "C" _declspec(dllexport) int returnNumber2(void);
+extern "C" _declspec(dllexport) char* returnString(void);
+extern "C" _declspec(dllexport) int sum(int a, int b);
